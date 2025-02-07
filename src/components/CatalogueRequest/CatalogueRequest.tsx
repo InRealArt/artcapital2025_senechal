037388abb9844fbb6f9fbd5d9b4c10f6d5c84ac2
gilbruno <gilles_bruno@gmx.fr>
2025-02-07 20:21:24 +0100

    img catalog

--- src/components/CatalogueRequest/CatalogueRequest.tsx
+++ src/components/CatalogueRequest/CatalogueRequest.tsx
@@ -24,7 +24,7 @@ const CatalogueRequest = () => {
   return (
     <div className={styles.container} id="catalogueRequest">
       <Image
-        src="/img/catalogueRequest/inrealartCatalogue.png"
+        src="/img/catalogueRequest/inrealartCatalogArtCapital.png"
         alt="Catalogue InRealArt"
         className={styles.imageSection}
         width={400}
